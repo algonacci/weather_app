b70e29e522ba40cd7384ab4d761c4f14a5e311f7
algonacci <86970816+algonacci@users.noreply.github.com>
2021-07-14 23:40:03 +0700

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'weather_api_client.dart';
 
-Future<void> main(List<String> arguments) async {
+Future<void> main(List<String> arguments) async 
   if (arguments.length != 1) {
     print('Syntax: dart bin/main.dart <city>');
     return;
